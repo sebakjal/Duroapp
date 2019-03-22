a63c7b8be7f1d05f427f218976c0a4dd9a22f3c9
k_jal <seba.carvajal.7@gmail.com>
2019-03-21 23:34:37 -0300

    Esta bien esta wea?

--- app/src/main/java/com/example/duroapp/MainActivity.java
+++ app/src/main/java/com/example/duroapp/MainActivity.java
@@ -4,12 +4,10 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         //getActionBar().hide();
 
 
-
+///
 
         Intent mServiceIntent = new Intent(MainActivity.this, ServiceNoDelay.class);
         Toast.makeText(getApplicationContext(), "if efuera", Toast.LENGTH_SHORT).show();
